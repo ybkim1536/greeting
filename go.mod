c111634c4eac35ed2d943ae20e9d5d5b6534420a
kyb1536 <kyb1536@gmail.com>
2021-03-11 22:56:09 +0900

    go module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ybkim1536/greeting
+
+go 1.16
---
+++ greeting2.go
@@ -0,0 +1,10 @@
+package greeting
+
+import "fmt"
+
+
+func Goodby(name string) string {
+	message := fmt.Sprintf(Goodby, %v. !", name)
+	return message
+
+}
